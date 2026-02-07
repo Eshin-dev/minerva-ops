3003b0d869bfc655765ecdadc35abfc16105b549
sddev12 <sd.dev@outlook.com>
2026-02-07 23:27:56 +0000

    re-scaffold

---
+++ footwell-podiatry/src/collections/Pages.ts
@@ -0,0 +1,51 @@
+import { CollectionConfig } from 'payload'
+
+export const Pages: CollectionConfig = {
+  slug: 'pages',
+  admin: {
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'layout',
+      type: 'blocks',
+      required: true,
+      blocks: [
+        {
+          slug: 'hero',
+          fields: [
+            {
+              name: 'heading',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'subheading',
+              type: 'text',
+            },
+          ],
+        },
+        {
+          slug: 'textBlock',
+          fields: [
+            {
+              name: 'content',
+              type: 'richText',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}

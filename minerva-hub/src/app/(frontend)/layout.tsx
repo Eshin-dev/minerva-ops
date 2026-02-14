c3d13d37ace112f81d7746e96f17c49a1a9c6095
sddev12 <sd.dev@outlook.com>
2026-02-14 16:10:45 +0000

    chore: add tailwind frontend

--- minerva-hub/src/app/(frontend)/layout.tsx
+++ minerva-hub/src/app/(frontend)/layout.tsx
@@ -11,8 +11,8 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
+      <body className="min-h-screen antialiased">
+        <main className="min-h-screen">{children}</main>
       </body>
     </html>
   )
